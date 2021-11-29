144fc24dde1672ebddb58ce84d222be693afcccb
João Augusto <johhdq@hotmail.com>
2021-11-29 11:15:11 -0300

    Entities are being created

---
+++ source/repos/WorkDay/Criado/Entities/WorkItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Criado.Entities
+{
+    class WorkItem
+    {
+        public string Description { get; set; }
+        public int CodItem { get; set; }
+        public int Category { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public WorkItem(string description, DateTime start, DateTime finish)
+        {
+            Description = description;
+            Start = start;
+            Finish = finish;
+        }
+
+        public int TotalTime()
+        {
+            TimeSpan duration = Finish.Subtract(Start);
+
+            if (duration.TotalMinutes >= 60 && duration.TotalHours < 24)
+            {
+                return (int)duration.TotalHours;
+            }
+            else if (duration.TotalMinutes < 60)
+            {
+                return (int)duration.TotalMinutes;
+            }
+            else
+            {
+                return (int)duration.Days;
+            }
+        }
+        public void RemoveItem(List<WorkItem> workItems, int codItem)
+        {
+            var item = workItems.Find(x => x.CodItem == codItem);
+            if (item != null)
+            {
+                workItems.Remove(item);
+            }
+        }
+        public void AddItem(List<WorkItem> workItems, WorkItem item)
+        {
+            var items = workItems.Find(x => x.CodItem == item.CodItem);
+            if (items == null)
+            {
+                workItems.Add(item);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Category" + Category +  "Description >> " + Description + "; Start >> " + Start + "; Finish >> " + Finish
+                + "Work Time >> " + TotalTime();
+        }
+    }
+}
+
---
+++ source/repos/WorkDay/Criado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Criado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
